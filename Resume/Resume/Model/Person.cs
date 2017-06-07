908608603c80f534ebe9dc92635a606e4563738a
jsmith302 <jsmith302@cnm.edu>
2017-06-06 21:16:21 -0600

    resume proj

---
+++ Resume/Resume/Model/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Resume.Model
+{
+    public class Person
+    {
+        public string FistName { get; set; }
+        [Required()]
+        public string LastName { get; set; }
+        [Required()]
+        public string Phone { get; set; }
+        [Required()]
+        [DataType(DataType.PhoneNumber)]
+        public string EmailAddress { get; set; }
+        [Required()]
+        [DataType(DataType.EmailAddress)]
+        public string Address { get; set; }
+
+    }
+}
