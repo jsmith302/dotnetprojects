7cffa6209ebf477609d19464bbb18b694f6f2205
jsmith302 <jsmith302@cnm.edu>
2017-06-06 21:21:33 -0600

    MVC proj

---
+++ MVCMovie - Copy/MVCMovie/Models/Movie.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        [Required]
+        [StringLength(60, MinimumLength = 1, ErrorMessage ="Please, enter a movie title." )]
+        public string Title { get; set; }
+        [DisplayFormat(DataFormatString="{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        [Display(Name ="Release Date")]
+        [Range(typeof(DateTime), "1/1/1900", "1/1/2020")]
+        public DateTime ReleaseDate { get; set; }
+        [Required]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$", 
+            ErrorMessage =("Please enter a genre starting with an uppercase letter: Action"))]
+
+        public string Genre { get; set; }
+
+        [Required]
+        [Range(.01, 100)]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$", ErrorMessage = "Please enter a genre starting with an uppercase letter: Action")
+            ]
+        [StringLength(5)]
+        public string Rating { get; set; }
+
+        //^ = beginning of line
+        //[A-Z] first character must be upper case
+        //+ and
+        //[a-z or A-Z or ' or - or space
+        //* any number of times
+        //$ end of line
+
+    }
+}
---
+++ MVCMovie - Copy/MVCMovie/Models/SeedData.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCMovie.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MVCMovieContext(
+                serviceProvider.GetRequiredService<DbContextOptions
+                <MVCMovieContext>>()))
+            {
+                if (context.Movie.Any())
+                {
+                    return;
+                }
+                foreach (var movie in context.Movie)
+                {
+                    context.Movie.Remove(movie);
+                }
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "When Harry Met Sally",
+                        ReleaseDate = DateTime.Parse("1989-01-11"),
+                        Genre = "Romantic Comedy",
+                        Price = 7.99M,
+                        Rating = "R"
+                    },
+                      new Movie
+                      {
+                          Title = "Ghostbusters",
+                          ReleaseDate = DateTime.Parse("1984-03-13"),
+                          Genre = "Comedy",
+                          Price = 7.99M,
+                          Rating = "PG-13"
+                      },
+                        new Movie
+                        {
+                            Title = "HItch",
+                            ReleaseDate = DateTime.Parse("2000-02-14"),
+                            Genre = "Romantic Comedy",
+                            Price = 7.99M,
+                            Rating = "PG-13"
+                        },
+                          new Movie
+                          {
+                              Title = "Halloween",
+                              ReleaseDate = DateTime.Parse("1990-10-31"),
+                              Genre = "Horror",
+                              Price = 7.99M,
+                              Rating = "R"
+                          });
+                context.SaveChanges();
+            }
+        }
+    }
+}
+
+
